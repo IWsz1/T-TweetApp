0f04817ea0e1df631ee1ee4ed329a9314828bb16
IWsz1 <soraa24926@gmail.com>
2023-10-28 20:28:19 +0900

    です。　comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
   end
   private
   def comment_params
-    # permitはフォームで入力された情報のみのためtextのみ
+    # permitはフォームで入力された情報のみのためtextのみです。
     # paramsのtweet_idには送付元詳細ページのuriパターンの中のtweet_idに当てはまる数字が入っている（ツイートテーブルのidがそこには入る設定）
     params.require(:comment).permit(:text).merge(user_id: current_user.id,tweet_id:params[:tweet_id])
   end
